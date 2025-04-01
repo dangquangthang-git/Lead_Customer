a5416de1a9a818155293566d2f47b7b1b1aaf07a
Lead_Customer <dangquangthang.work@gmail.com>
2025-04-01 22:36:28 +0700

    Commit03

--- src/test/java/company/Testcase_Lead_Customer.java
+++ src/test/java/company/Testcase_Lead_Customer.java
@@ -2,6 +2,7 @@ package company;
 
 import commons.BaseTest;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -13,5 +14,15 @@ public class Testcase_Lead_Customer extends BaseTest {
     @BeforeTest
     public void before(String browserName, String url) {
         driver = getBrowserDriver(browserName, url);
+    }
+
+    @Test
+    public void testcase() {
+        System.out.println("con gà");
+    }
+
+    @AfterTest
+    public void after() {
+        driver.close();
     }
 }

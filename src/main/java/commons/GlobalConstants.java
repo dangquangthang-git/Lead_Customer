aaadba3804abecdac168bae0a58e4b1fb63022ae
Thang Dang Quang (IPAS HO_DSB) <thang.dangquang@ivnd.com.vn>
2025-04-03 16:23:19 +0700

    remove redundant code

--- src/main/java/commons/GlobalConstants.java
+++ src/main/java/commons/GlobalConstants.java
@@ -5,7 +5,7 @@ public class GlobalConstants {
     public static final String SEPARATOR = System.getProperty("file.separator");
     public static final String EXTENT_PATH = PROJECT_PATH + SEPARATOR + "htmlExtent" + SEPARATOR;
     public static final String JAVA_VERSION = System.getProperty("java.version");
-//d
+
     public static final long SHORT_TIMEOUT = 10;
     public static final long LONG_TIMEOUT = 30;
 
